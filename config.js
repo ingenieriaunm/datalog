abc308e1963bb1582748046e1900f35fd448e176
ingenieriaunm <acasio.7c@gmail.com>
2025-03-24 23:47:17 -0300

    publish web

---
+++ config.js
@@ -0,0 +1,13 @@
+export const imageUrls = [
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/1.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/2.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/3.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/4.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/5.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/6.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/7.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/8.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/9.jpg',
+    'https://raw.githubusercontent.com/ingenieriaunm/datalog/main/dataImg/10.jpg',
+];
+
